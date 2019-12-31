54351b7bd94a9874b50f59b9109171e1df63dd46
jonmpan <jonmpan@gmail.com>
2019-12-30 17:32:41 -0700

    added words and a test madlib

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-const madLibs = require("./mad-libs.json").madLibs;
-const words = require("./random-words.json");
+const madLibs = require("./madLibs.json").madLibs;
+const words = require("./words.json");
 words.celebrity = words.celebrityF.concat(words.celebrityM);
 words.name = words.nameF.concat(words.nameM);
 
@@ -53,6 +53,15 @@ var inputs = [
   { id: "adj2", value: "skinny" },
   { id: "noun3", value: "litter box" }
 ];
+
+var sanitize = madLibs => {
+  return madLibs.map(o => {
+    o.text = o.text.replace(/<br>/g, "\n");
+    return o;
+  });
+};
+
+console.log(sanitize(madLibs));
 
 // console.log(inputs);
 // console.log(createMadLib(madLibs.madLibs[0].text, inputs));
